b31641087a6b5b2ea03fa77cd753e1479551b623
waseemnawaz <waseemhijodenawaz@yahoo.es>
2018-09-21 23:09:18 +0200

    added color and price to products

---
+++ db/migrate/20180921203259_rename_colors_in_products.rb
@@ -0,0 +1,4 @@
+class RenameColorsInProducts < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "One more pic from Cologne", description: "What a nice pic thhat is!", image_url: "https://i2.wp.com/experimentalwebsiteforme.com/wp-content/uploads/2018/06/coloniacieloazul.jpg?resize=768%2C576&ssl=1")
