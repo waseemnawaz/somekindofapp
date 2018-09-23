8ef62cd27007888e6ba0e199b03af3c1c48ec1de
waseemnawaz <waseemhijodenawaz@yahoo.es>
2018-09-24 00:20:05 +0200

    carousel

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -4,5 +4,7 @@ class SimplePagesController < ApplicationController
 
   def landing_page
   	@featured_product = Product.first
+  	#@products = Product.limit(3)
+  	@products = Product.offset(rand(Product.count)).limit(4)
   end
 end
