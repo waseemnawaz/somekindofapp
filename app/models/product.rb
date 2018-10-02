78ead08a07214a5d7eb97c0c1fdea852180c1600
waseemnawaz <waseemhijodenawaz@yahoo.es>
2018-10-02 16:50:25 +0200

    comment model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,14 @@
 class Product < ApplicationRecord
 	has_many :orders
+	has_many :comments
+
+	def highest_rating_comment
+		comments.rating_desc.first
+	end
+
+	def lowest_rating_comment
+		comments.rating_asc.first
+	end
 
 	def self.search(search_term)
 		if (Rails.env.development?)
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   get 'simple_pages/landing_page'
   post 'simple_pages/thank_you'
-  root 'products#index'
+  root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
